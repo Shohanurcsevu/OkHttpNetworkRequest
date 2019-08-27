645d3d6d6da4ab0f2c65018a63a4457f47dfc046
Shohanur Rahman <shohanurvu@gmail.com>
2019-08-27 18:35:55 +0600

    Final Commit

--- app/src/main/java/com/example/okhttpnetworkrequest/MainActivity.java
+++ app/src/main/java/com/example/okhttpnetworkrequest/MainActivity.java
@@ -3,12 +3,57 @@ package com.example.okhttpnetworkrequest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView=(TextView)findViewById(R.id.text_view_result);
+
+        OkHttpClient okHttpClient=new OkHttpClient();
+        String url="https://reqres.in/api/users?page=2";
+
+        Request request=new Request.Builder()
+                .url(url).build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+
+                if(response.isSuccessful())
+                {
+                    final String myResponse=response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextView.setText(myResponse);
+                        }
+                    });
+                }
+
+            }
+        });
+
     }
 }
